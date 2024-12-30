0098716e524ad10f3d74e0c5ba905f16d8e8557a
Georg Eichler <georg_eichler@web.de>
2024-12-30 19:55:31 +0100

    Adding plot for velocity gain for each additional stage

--- similar_stage_rocket.py
+++ similar_stage_rocket.py
@@ -10,11 +10,55 @@ Moreover they all have the same specific impulse I_sp
 g0 = 9.81    # in m/s
 I_sp = 300   # in s
 
-r = 0.05     # ratio of payload/total mass
-epsilon = 0.15
+r_array = [0.01, 0.05]     # ratio of payload/total mass
+epsilon_array = [0.05, 0.1, 0.15]
 
 num_stages = 10
 stages = range(1, num_stages+1)
+
+
+
+for r in r_array:
+    v_differences = np.zeros((len(epsilon_array), num_stages))
+    i = 0
+    for epsilon in epsilon_array:
+        v_stages = np.array([])
+
+        for n in stages:
+            v_end = I_sp*g0*n*np.log(1/(r**(1/n) * (1 - epsilon) + epsilon))
+            v_stages = np.append(v_stages, v_end)
+
+        v_max = I_sp*g0*(1-epsilon)*np.log(1/r)
+        
+        #Calculate velocity gain per stage
+        v_differences[i] = np.insert(np.diff(v_stages), 0, v_stages[0])
+        #Normalise to maximal velocity
+        v_differences[i] = v_differences[i]/v_max
+
+        #Update i for next row
+        i = i+1
+
+
+        plt.scatter(stages, v_stages/v_max, label = f'$\epsilon = $ {epsilon}, $\lambda$ = {r}')
+        plt.plot(stages, v_stages/v_max)
+
+    plt.xlabel('Number of stages')
+    plt.ylabel('Final velocity to maximal velocity')
+    plt.title('Final velocity for stage similar rocket')
+    plt.legend()
+    plt.figure()
+    
+    
+    for k in range(len(epsilon_array)):
+        plt.scatter(stages, v_differences[k], label = f'$\epsilon = $ {epsilon_array[k]}, $\lambda$ = {r}')
+        #plt.plot(stages, v_differences[k])
+    
+    plt.xlabel('Number of stages')
+    plt.ylabel('Velocity gain to the last stage')
+    plt.legend()
+    plt.figure()
+
+'''
 v_stages = np.array([])
 
 for n in stages:
@@ -27,9 +71,9 @@ plt.scatter(stages, v_stages/v_max, label = f'$\epsilon = $ {epsilon}, r = {r}')
 plt.plot(stages, v_stages/v_max)
 plt.xlabel('Number of stages')
 plt.ylabel('Final velocity to maximal velocity')
-plt.title('Final velocity for stage similar rocket per stage')
+plt.title('Final velocity for stage similar rocket')
 plt.legend()
-
+'''
 
 #Calculating necessary payload/total mass ratio for target velocity
 v_target = 10*10**3 #target velocity (m/s)
@@ -54,7 +98,7 @@ r_array = np.array(r_array)
 r_array = np.where(r_array < 0, np.nan, r_array)
  
 
-plt.figure()
+#plt.figure()
 plt.scatter(stages, r_array, label = fr'$I_{{sp}}$ = {I_sp}, $\epsilon =$ {epsilon}, $v_{{target}}$ = {v_target/1000} km/s')
 plt.plot(stages, r_array)
 plt.xlim(stages[0], stages[-1])
