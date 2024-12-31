20a158c78a8bc9cae516a684160246278ce6c6d4
Georg Eichler <georg_eichler@web.de>
2024-12-31 12:19:59 +0100

    Plot for to m0 normalised stage masses

--- mass_optimisation_Lagrange_multiplier.py
+++ mass_optimisation_Lagrange_multiplier.py
@@ -15,6 +15,7 @@ def constraint_equation(x, v, I_sp, epsilon):
         return np.sum(c * np.log( (x*c - 1) / (epsilon*x*c) )) - v
 
 def Lagrange_mass_optimisation(v_end, I_sp, epsilon):
+    #v_end is given in km/s
     #Ensure arrays are numpy arrays
     I_sp = np.array(I_sp)
     epsilon = np.array(epsilon)
@@ -32,7 +33,9 @@ def Lagrange_mass_optimisation(v_end, I_sp, epsilon):
         mu = result.root
     else:
         raise ValueError("Root finding does not converge")
-    
+    #print("mu:",mu)
+    x0 = mu
+    #print("constraint terms",c * np.log( (x0*c - 1) / (epsilon*x0*c) ))
     n = (mu*c - 1) / (epsilon*mu*c)
 
     #get number of stage
@@ -54,10 +57,41 @@ def Lagrange_mass_optimisation(v_end, I_sp, epsilon):
     return m0, m, m_empty, m_fuel
 
 v_end = 10
-I_sp = [250, 250, 350]
+I_sp = [300, 300, 300]
 epsilon = [0.1, 0.1, 0.1]
-m_payload = 5000
 
 #for given payload mass the total mass is given by multiplying by m_payload
 m0, m, m_empty, m_fuel = Lagrange_mass_optimisation(v_end, I_sp, epsilon)
-print(m)
+print(1/m0*m)
+
+#setting fontsize
+fontsize_label = 14
+fontsize_legend = 14
+fontsize_ticks = 14
+
+epsilon_array = np.linspace(0.05, 0.15, 11)
+I_sp_array = [300]
+
+#number of stages
+s = 3
+
+for x in I_sp_array:
+    I_sp = x*np.ones(s)
+    m_normalised = np.zeros((s, len(epsilon_array)))
+
+    for j, eps in enumerate(epsilon_array):
+        epsilon = eps*np.ones(s)
+        m0, m, m_empty, m_fuel = Lagrange_mass_optimisation(v_end, I_sp, epsilon)
+
+        #normalise m
+        m = 1/m0*m 
+
+        #Store values
+        m_normalised[:,j] = m
+
+    #Plot the normalised values
+    for k in range(s):
+        plt.plot(epsilon_array, m_normalised[k, :])
+    print(m_normalised)
+
+plt.show()
